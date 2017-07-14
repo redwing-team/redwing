747af6ef206de70ceb06a35d5e204b8d3aaedeae
ken1987 <ken198701@gmail.com>
2017-07-14 11:29:32 +0800

    add:  postcss-px2rem配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = () => ({
     plugins: {
+        'postcss-px2rem': {
+            remUnit: 75
+        },
         autoprefixer: {
             browsers: ['ie >= 9', '> 5% in CN', 'last 3 versions']
         }
