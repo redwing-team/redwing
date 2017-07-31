53085ebf4cb5762d1bdd2c9bd25831c53c4468c5
ken1987 <ken198701@gmail.com>
2017-07-31 10:39:43 +0800

    fix: editor 组件  删除标签之间的空白

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -11,11 +11,15 @@ export default {
         paste(e) {
             e.preventDefault()
             const html = e.clipboardData.getData('text/html')
-            document.execCommand('insertHTML', false, html)
+            document.execCommand(
+                'insertHTML',
+                false,
+                html.replace(/>\s+</g, '><')
+            )
             this.input()
         },
         input() {
-            const v = this.$el.innerHTML
+            const v = this.$el.innerHTML.replace(/[\n\r]/gi, '<br>')
             this.html = v
             this.$emit('input', v)
         }
--- src/pages/editor/index.js
+++ src/pages/editor/index.js
@@ -6,8 +6,7 @@ export default {
     },
     data() {
         return {
-            content:
-                '<!--StartFragment--><h1 class="postTitle" style="margin-top: 0px; margin-bottom: 0px; padding-left: 5px; font-size: 18.2px; border-bottom: 1px solid rgb(153, 153, 153); float: left; line-height: 2em; width: 1583px; clear: both; color: rgb(0, 0, 0); font-family: Verdana, Arial, Helvetica, sans-serif;"><a id="cb_post_title_url" class="postTitle2" href="http://www.cnblogs.com/Songyc/p/4458570.html" style="color: rgb(51, 153, 0); transition: all 0.4s linear 0s;">b的用法</a></h1><div class="clear" style="clear: both; color: rgb(0, 0, 0); font-family: Verdana, Arial, Helvetica, sans-serif;"></div><div class="postBody" style="padding: 15px 2px 5px 5px; line-height: 1.5; color: rgb(0, 0, 0); border-bottom: 1px solid rgb(204, 204, 204); font-family: Verdana, Arial, Helvetica, sans-serif;"><div id="cnblogs_post_body" style="margin-bottom: 20px; word-break: break-word;"><p style="margin: 10px auto;">getBoundingCli</p><!--StartFragment--><h1 class="postTitle" style="margin-top: 0px; margin-bottom: 0px; padding-left: 5px; font-size: 18.2px; border-bottom: 1px solid rgb(153, 153, 153); float: left; line-height: 2em; width: 1583px; clear: both;">a</h1><!--EndFragment--><p style="margin: 10px auto;">enstdRect</p></div></div><!--EndFragment--><!--EndFragment-->'
+            content: '<p>1</p><p>2<p>'
         }
     }
 }
