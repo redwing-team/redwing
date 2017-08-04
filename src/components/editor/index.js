8b6653b237bf6206ad583bd2fc95a525b0d344cb
ken1987 <ken198701@gmail.com>
2017-08-04 14:03:42 +0800

    fix: document.execCommand 引起部分场景下丢失标签属性的问题 https://segmentfault.com/n/1330000010502826

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -1,3 +1,36 @@
+function pasteHtmlAtCaret(html) {
+    let range
+    const sel = window.getSelection()
+
+    if (sel.getRangeAt && sel.rangeCount) {
+        range = sel.getRangeAt(0)
+        range.deleteContents()
+
+        // Range.createContextualFragment() would be useful here but is
+        // non-standard and not supported in all browsers (IE9, for one)
+        const el = document.createElement('div')
+        el.innerHTML = html
+        const frag = document.createDocumentFragment()
+        let node = el.firstChild
+        let lastNode
+
+        while (node) {
+            lastNode = frag.appendChild(node)
+            node = el.firstChild
+        }
+        range.insertNode(frag)
+
+        // Preserve the selection
+        if (lastNode) {
+            range = range.cloneRange()
+            range.setStartAfter(lastNode)
+            range.collapse(true)
+            sel.removeAllRanges()
+            sel.addRange(range)
+        }
+    }
+}
+
 export default {
     props: {
         value: String
@@ -10,8 +43,17 @@ export default {
     methods: {
         paste(e) {
             e.preventDefault()
-            const html = e.clipboardData.getData('text/html')
-            document.execCommand('insertHTML', false, html)
+
+            const c = e.clipboardData
+            const types = c.types
+            let type = 'text/plain'
+
+            if (types.some(item => item === 'text/html')) {
+                type = 'text/html'
+            }
+
+            const html = e.clipboardData.getData(type)
+            pasteHtmlAtCaret(html)
             this.input()
         },
         input() {
